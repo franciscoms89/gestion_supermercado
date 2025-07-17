f9983230eaa96a6e4803dc0a688b589ffe7a16a1
Francisco José Morcillo Suárez <francisco.morcillo89@gmail.com>
2025-07-17 20:16:36 +0200

    creado proyecto con dependencias

---
+++ gestion-ventas/src/test/java/com/supermercado/gestion_ventas/GestionVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermercado.gestion_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
