3189df225f734cf05c1081f57dadd0dd5a071d88
Julio Castro Fernandez <juliocastrofernandez11@gmail.com>
2025-07-17 21:48:19 +0200

    Models, DTOs y configuracion para MySQL

--- gestion-ventas/src/main/java/com/supermercado/gestion_ventas/dtos/SaleDTO.java
+++ gestion-ventas/src/main/java/com/supermercado/gestion_ventas/dtos/SaleDTO.java
@@ -1,4 +1,28 @@
 package com.supermercado.gestion_ventas.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SaleDTO {
+    private Long id;
+    private Long shopId;
+    private LocalDate saleDate;
+    private List<SaleDetailsDTO> saleDetails;
+
+
+    // DTO interno para el desglose de cada producto en la venta
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SaleDetailsDTO{
+        private Long productId;
+        private int quantity;
+    }
 }
--- gestion-ventas/src/main/java/com/supermercado/gestion_ventas/models/Sale.java
+++ gestion-ventas/src/main/java/com/supermercado/gestion_ventas/models/Sale.java
@@ -1,4 +1,36 @@
 package com.supermercado.gestion_ventas.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne // Relación: Muchas Ventas pertenecen a una Sucursal
+    @JoinColumn(name = "shop_id", nullable = false)
+    private Shop shop;
+
+    private LocalDate saleDate;
+
+    //Relación: Una Venta contiene muchos Productos
+    //Una Venta puede tener muchos Productos y un Producto puede estar en muchas Ventas
+    @ManyToMany
+    @JoinTable(
+            name = "sale_products",
+            joinColumns = @JoinColumn(name = "sale_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+            )
+    private Set<Product> products = new HashSet<>();
 }
