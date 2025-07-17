a11101d8fc8b188a83cb9c3ffe4d2848e8601669
Francisco José Morcillo Suárez <francisco.morcillo89@gmail.com>
2025-07-17 21:56:03 +0200

    creado ProductController

--- gestion-ventas/src/main/java/com/supermercado/gestion_ventas/controllers/ProductController.java
+++ gestion-ventas/src/main/java/com/supermercado/gestion_ventas/controllers/ProductController.java
@@ -1,4 +1,50 @@
 package com.supermercado.gestion_ventas.controllers;
 
+import com.supermercado.gestion_ventas.dtos.ProductDTO;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+
+    // Todo: Todos los nombres de las funciones están sujetos a cambios
+
+    // IoC
+
+    @Autowired
+    ProductInterfaz productService;
+
+    // GET
+    @GetMapping("/productos")
+    public ResponseEntity<List<ProductDTO>> listAll() {
+        List<ProductDTO> list = productService.listAll();
+        return ResponseEntity.ok(list);
+    }
+
+    // POST
+    @PostMapping("/{id}")
+    public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO product) {
+        ProductDTO productoCreated = productService.create(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoCreated);
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productToUpdate) {
+        ProductDTO productDTO = productService.update(id, productToUpdate);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(productDTO);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<ProductDTO>> delete(@PathVariable Long id) {
+        List<ProductDTO> list = productService.delete(id);
+        return ResponseEntity.ok(list);
+    }
 }
