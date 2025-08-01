c34232a8a2e054b5171622979b231cf3edc2c78a
JulioCastroFernandez <juliocastrofernandez11@gmail.com>
2025-08-01 16:57:49 +0200

    Update ProductController.java

--- gestion-ventas/src/main/java/com/supermercado/gestion_ventas/controllers/ProductController.java
+++ gestion-ventas/src/main/java/com/supermercado/gestion_ventas/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package com.supermercado.gestion_ventas.controllers;
 
 import com.supermercado.gestion_ventas.dtos.ProductDTO;
-import com.supermercado.gestion_ventas.response.ApiResponse;
 import com.supermercado.gestion_ventas.services.interfaces.ProductInterfaz;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -25,50 +23,27 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<?> listAll() {
         List<ProductDTO> productList = productService.listAll();
-        if (productList.isEmpty()) {
-            ApiResponse response = new ApiResponse(
-                    "No hay productos registrados",
-                    HttpStatus.NO_CONTENT.value(),
-                    LocalDate.now()
-                    );
-            return ResponseEntity.ok(response);
-        }
         return ResponseEntity.ok(productList);
     }
 
     // POST - Crear un nuevo producto
     @PostMapping
-    public ResponseEntity<ApiResponse> create(@RequestBody ProductDTO product) {
+    public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO product) {
         ProductDTO productCreated = productService.create(product);
-        ApiResponse response = new ApiResponse(
-                "Producto creado exitosamente",
-                HttpStatus.CREATED.value(),
-                LocalDate.now()
-        );
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreated);
     }
 
     // PUT - Actualizar un producto existente
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse> update(@PathVariable Long id, @RequestBody ProductDTO productToUpdate) {
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productToUpdate) {
         ProductDTO updateProduct = productService.update(id, productToUpdate);
-        ApiResponse response = new ApiResponse(
-                "Producto actualizado exitosamente",
-                HttpStatus.OK.value(),
-                LocalDate.now()
-        );
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(updateProduct);
     }
 
     // DELETE - Eliminar un producto
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.delete(id);
-        ApiResponse response = new ApiResponse(
-                "Producto eliminado exitosamente",
-                HttpStatus.NO_CONTENT.value(),
-                LocalDate.now()
-        );
-        return ResponseEntity.ok(response);
+        return ResponseEntity.noContent().build();
     }
 }
