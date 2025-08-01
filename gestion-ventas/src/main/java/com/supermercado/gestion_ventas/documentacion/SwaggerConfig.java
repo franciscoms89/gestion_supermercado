4233cda680044b76a4575525dfbfc00caa480f0c
Iago Rivas <iagorivas@gmail.com>
2025-08-01 15:01:29 +0200

    implementado_swagger

---
+++ gestion-ventas/src/main/java/com/supermercado/gestion_ventas/documentacion/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.supermercado.gestion_ventas.documentacion;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean  // Esta etiqueta es necesaria para registrar el bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Una cadena de supermercados")
+                        .version("0.0.1")
+                        .description("Se gestionaran ventas , ofertas y tiendas de la cadena"));
+    }
+}
