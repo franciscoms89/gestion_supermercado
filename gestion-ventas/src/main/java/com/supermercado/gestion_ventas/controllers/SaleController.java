8d1f44b70b2eeed80599702d826937f859da7da4
JulioCastroFernandez <juliocastrofernandez11@gmail.com>
2025-08-01 16:58:26 +0200

    Update SaleController.java

--- gestion-ventas/src/main/java/com/supermercado/gestion_ventas/controllers/SaleController.java
+++ gestion-ventas/src/main/java/com/supermercado/gestion_ventas/controllers/SaleController.java
@@ -1,7 +1,6 @@
 package com.supermercado.gestion_ventas.controllers;
 
 import com.supermercado.gestion_ventas.dtos.SaleDTO;
-import com.supermercado.gestion_ventas.response.ApiResponse;
 import com.supermercado.gestion_ventas.services.interfaces.SaleInterfaz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,42 +21,24 @@ public class SaleController {
 
     // GET - Obtener todas las ventas con posibles filtrados
     @GetMapping
-    public ResponseEntity<?> listAll(@RequestParam(name = "tiendaId", required = false) Long tiendaId,
+    public ResponseEntity<List<SaleDTO>> listAll(@RequestParam(name = "tiendaId", required = false) Long tiendaId,
                                                  @RequestParam(name = "fecha", required = false) LocalDate fechaOferta) {
         // TODO [IMPORTANTE]: variables en español para hacer las busquedas, ya que los endPoints estan en español.
         List<SaleDTO> saleList = saleService.listAll(tiendaId, fechaOferta);
-        if (saleList.isEmpty()) {
-            ApiResponse response = new ApiResponse(
-                    "No hay ventas registradas",
-                    HttpStatus.OK.value(),
-                    LocalDate.now()
-            );
-            return ResponseEntity.ok(response);
-        }
         return ResponseEntity.ok(saleList);
     }
 
     // POST - Crear una nueva venta
     @PostMapping
-    public ResponseEntity<ApiResponse> create(@RequestBody SaleDTO sale) {
+    public ResponseEntity<SaleDTO> create(@RequestBody SaleDTO sale) {
         SaleDTO saleCreated = saleService.register(sale);
-        ApiResponse response = new ApiResponse(
-                "Venta registrada exitosamente",
-                HttpStatus.CREATED.value(),
-                LocalDate.now()
-        );
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saleCreated);
     }
 
     // DELETE - Eliminar una venta
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         saleService.delete(id);
-        ApiResponse response = new ApiResponse(
-                "Venta eliminada exitosamente",
-                HttpStatus.OK.value(),
-                LocalDate.now()
-        );
-        return ResponseEntity.ok(response);
+        return ResponseEntity.noContent().build();
     }
 }
